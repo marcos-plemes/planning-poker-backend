7304deda091f1e3a037a6a460f461bfc8904f867
Marcos Vinicius <mvplemes@gmail.com>
2024-10-25 09:33:56 -0300

    adiciona web

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
@@ -9,15 +8,63 @@ const io = require('socket.io')(server, {
     }
 });
 
+let jogadoresConectados = [];
+
+let jogadoresComCartas = [];
+
 
 app.get('/', (req, res) => {
-    res.send('Ola, Yanka')
+    res.send('Ola, Mundo')
 })
 
 io.on('connection', socket => {
-    console.log(socket.id)
+    console.log(`Usuário conectado: ${socket.id}`);
+    socket.emit("jogadoresConectados", jogadoresConectados);
 
-    socket.emit("teste", "Teste enviado pelo servidor!");
+    socket.on('nomeDoJogador', nome => {
+        const jogador = {id: socket.id, nome: nome};
+        jogadoresConectados.push(jogador)
+        io.emit('jogadorConectado', jogador)
+    })
+
+    socket.on('tituloDaCarta', tituloDaCarta => {
+        const jogadorIndex = jogadoresConectados.findIndex(jogador => jogador.id === socket.id);
+        if (jogadorIndex !== -1) {
+            if(tituloDaCarta) {
+                jogadoresConectados[jogadorIndex].isCartaSelecionada = true;
+
+            } else {
+                jogadoresConectados[jogadorIndex].isCartaSelecionada = false;
+            }
+            
+            jogador = jogadoresConectados[jogadorIndex];
+            jogador.tituloDaCarta = tituloDaCarta;
+
+            jogadoresComCartas = jogadoresComCartas.filter(jogador => jogador.id !== socket.id);
+            jogadoresComCartas.push(jogador)
+            console.log(jogadoresConectados)
+            io.emit('jogadorEscolheuUmaCarta', jogador)
+        }
+
+    })
+
+    socket.on('revelar-cartas', () => {
+        io.emit('cartas-escolhidas', jogadoresComCartas)
+    })
+
+    socket.on('reiniciar-jogo', () => {
+        io.emit('reiniciar-jogo', {})
+    })
+
+    socket.on('disconnect', () => {
+        const jogadorDesconectado = jogadoresConectados.find(jogador => jogador.id === socket.id);
+
+        if (jogadorDesconectado) {
+            console.log(`Jogador desconectado: ${jogadorDesconectado.nome} (${socket.id})`);
+            jogadoresConectados = jogadoresConectados.filter(jogador => jogador.id !== socket.id);
+            io.emit('jogadorDesconectado', { id: jogadorDesconectado.id, nome: jogadorDesconectado.nome });
+        }
+    });
 
 })
 
